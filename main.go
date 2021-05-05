c41c0d1654222deca1c0bf23139fc20aaf8a2362
zzokki81 <zoran.rebic@outlook.com>
2021-05-05 16:11:54 +0200

    Add / endpoint

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"text/template"
 
 	"github.com/gorilla/mux"
 )
@@ -10,6 +11,24 @@ import (
 func main() {
 	r := mux.NewRouter()
 
+	fs := http.FileServer(http.Dir("./static/"))
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", fs))
+
+	r.HandleFunc("/", index).Methods("GET")
 	log.Println("Listening at port :8080....")
 	log.Fatal(http.ListenAndServe(":8080", r))
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	render(w, "static/templates/index.html", nil)
+}
+
+func render(w http.ResponseWriter, filename string, data interface{}) {
+	tmpl, err := template.ParseFiles(filename)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	if err := tmpl.Execute(w, data); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
